e03c9ae624ac9518f8bdf188d0e9643e4212f169
brunoferreira-hotmart <bruno.ferreira@hotmart.com>
2022-08-27 11:31:59 -0300

    first commit

---
+++ meuprimeiroprojetospringbatch/src/main/java/com/udemy/meuprimeiroprojetospringbatch/MeuprimeiroprojetospringbatchApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.meuprimeiroprojetospringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuprimeiroprojetospringbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuprimeiroprojetospringbatchApplication.class, args);
+	}
+
+}
---
+++ meuprimeiroprojetospringbatch/src/test/java/com/udemy/meuprimeiroprojetospringbatch/MeuprimeiroprojetospringbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.meuprimeiroprojetospringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuprimeiroprojetospringbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
